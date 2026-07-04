c3e3a45fd724295c497665eb628fc7a0b0f6e068
Ananda12 <ananda.xing12@gmail.com>
2026-07-04 22:56:38 +0800

    Server and client side real-time rendering simulation by operating SSE connections

--- scripts/server.js
+++ scripts/server.js
@@ -1,8 +1,56 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const mainPage = "index.html";
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`[SERVER]: The server is running on port ${3000}`);
+});
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/grids/:grid', (req, res) => {
+  const grid = req.params.grid;
+  console.log(`User is currently viewing grid: ${grid}`);
+});
+
+app.get('/clicked/:id', (req, res) => {
+  const id = req.params.id;
+  
+  // HTTP headers for SSE connection
+  res.writeHead(200, {
+    'content-type': 'text/event-stream',
+    'cache-control': 'no-cache',
+    'connection': 'keep-alive'
+  });
+
+  // SSE requires data to be prefixed with "data: " and end with two newlines
+  // Removing newlines from the HTML string so it doesn't break the SSE format
+  const sendUpdate = () => {
+    const htmlContent = `
+      <div class="profile-card" id="sse-wrapper-${id}">
+          <h2>Grid informations: </h2>
+          <p>Grid ID : ${id}</p>
+          <p>Status: Active</p>
+          <p>Time: ${new Date().toLocaleTimeString()} </p>
+      </div>
+    `;
+    const cleanHtml = htmlContent.replace(/\n/g, '').trim();
+    res.write(`data: ${cleanHtml}\n\n`);
+  }
+
+  const updateEverySecond = setInterval(() => {
+    sendUpdate();
+    console.log(`The content for id ${id} is updating for every 1 second`);
+  }, 1000);
+
+  req.on('close', () => {
+    console.log(`Connection closed for grid id: ${id}`)
+    clearInterval(updateEverySecond);
+    res.end();
+  });
 });
