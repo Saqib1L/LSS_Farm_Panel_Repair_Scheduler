38cee73314e876745ab7288ff7c7c6dfdd954ee3
Ananda12 <ananda.xing12@gmail.com>
2026-07-07 18:04:35 +0800

    Adding file detection features and adding zones within the data generation script

--- scripts/DataGenScript.js
+++ scripts/DataGenScript.js
@@ -1,10 +1,21 @@
 import { read } from 'fs';
 import fs from 'fs/promises';
-const filePath = "./data.json";
+const filePath = "../data.json";
+const totalData = 200000;
 
 const readData = async () => {
-  const rawData = await fs.readFile(filePath, 'utf8');
-  return JSON.parse(rawData);
+  try {
+    const rawData = await fs.readFile(filePath, 'utf8');
+    if(!rawData.trim()) {
+      return [];
+    }
+    return JSON.parse(rawData);
+  } catch (error) {
+    if(error.code === "ENOENT") {
+      console.log("[SERVER]: The data.json does not exists, the program is creating one...");
+      await fs.writeFile(filePath, JSON.stringify([], null, 2));
+    }
+  }
 }
 
 let dataArray = await readData();
@@ -15,14 +26,22 @@ const updateData = async () => {
 
 // Generating 200k data 
 const generateData = async () => {
+  let capacityReseter = 1;
+  let gridZones = 1;
 
-  for(let i=0; i<199999; i++) {
+  for(let i=0; i<totalData; i++) {
+    if(capacityReseter === 501) {
+      capacityReseter = 1;
+      gridZones++;
+    }
     await dataArray.push({
       "id": `SP${String(i+1).padStart(7, '0')}`,
       "efficiency": 100.00,
       "severity": "Healthy",
-      "maintenance_priority": "None"
+      "maintenance_priority": "None",
+      "zone": `Z-${gridZones}`
     });
+    capacityReseter++;
   }
 
   await updateData();
