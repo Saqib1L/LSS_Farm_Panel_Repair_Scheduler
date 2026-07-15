ad437d9c611492c5ded385b07e3a44cd2e326c3c
Saqib1L <muhammadsaqibka@gmail.com>
2026-07-15 18:24:29 +0800

    add GET /maintenance-queue route to serve the current   top-30 list as JSON

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from 'fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const mainPage = "index.html";
@@ -16,6 +17,17 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/grids/:grid', (req, res) => {
   const grid = req.params.grid;
   console.log(`User is currently viewing grid: ${grid}`);
+});
+
+app.get('/maintenance-queue', async (req, res) => {
+  try {
+    const rawData = await fs.readFile(path.join(__dirname, '..', 'sources', 'main_queue.json'), 'utf8');
+    const topPanels = JSON.parse(rawData);
+    res.json(topPanels);
+  } catch (error) {
+    console.log('[SERVER]: Could not read maintenance queue:', error.message);
+    res.json([]);
+  }
 });
 
 app.get('/clicked/:id', (req, res) => {
